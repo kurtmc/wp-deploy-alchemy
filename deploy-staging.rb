3a7dadbfb767d8bff851ac06a79cb5467ec0d3df
Kurt McAlpine <kurt@linux.com>
2016-02-28 11:44:24 +1300

    Deploy script for staging

---
+++ deploy-staging.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+#bundle exec cap staging deploy
+def exec_shell(cmd)
+    puts cmd
+    puts `#{cmd}`
+end
+def server(address, *args)
+    user = *args[0][:user]
+    user = user[0]
+    exec_shell("scp api-credentials.json #{user}@#{address}:/var/www/html/current/")
+    exec_shell("ssh #{user}@#{address} 'source ~/.bash_profile; cd /var/www/html/current; ./pull-json.rb'")
+end
+
+def set(*args)
+end
+
+exec_shell("bundle exec cap staging deploy")
+
+require './config/deploy/staging.rb'
