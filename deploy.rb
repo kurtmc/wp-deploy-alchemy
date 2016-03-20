28fa41e6dea7fb786b0eac4a14f4c57521506bfb
Kurt McAlpine <kurt@linux.com>
2016-03-20 17:44:21 +1300

    Updated deploy script

--- deploy.rb
+++ deploy.rb
@@ -17,8 +17,12 @@ def server(address, *args)
     user = user[0]
     exec_shell("scp api-credentials.json #{user}@#{address}:/var/www/html/current/")
     exec_shell("ssh #{user}@#{address} 'source ~/.bash_profile; cd /var/www/html/current; ./pull-json.rb'")
+    exec_shell("ssh #{user}@#{address} 'source ~/.bash_profile; cd /var/www/html/current; php api.php'")
     exec_shell("scp cron-jobs/* root@#{address}:/etc/cron.hourly/")
     exec_shell("ssh root@#{address} 'chmod +x /etc/cron.hourly/*'")
+    exec_shell("ssh #{user}@#{address} 'source ~/.bash_profile; cd /var/www/html/current; mkdir -p shared/db_backups; chmod 777 shared/db_backups'")
+    exec_shell("ssh #{user}@#{address} 'source ~/.bash_profile; cp /var/www/html/current/content/plugins/wp-dbmanager/htaccess.txt /var/www/html/current/.htaccess'")
+    exec_shell("ssh #{user}@#{address} 'source ~/.bash_profile; cp /var/www/html/current/content/plugins/wp-dbmanager/index.php /var/www/html/current/index.php'")
 end
 
 def set(*args)
