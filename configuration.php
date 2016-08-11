a53df2e8f926582bd092b1761c65b8a6559e18be
Kurt McAlpine <kurt@linux.com>
2016-08-11 21:14:13 +1200

    Fixed up configuration.php and deploy.rb

--- configuration.php
+++ configuration.php
@@ -3,7 +3,7 @@
 if (!function_exists('getConfiguration')) {
   function getConfiguration() {
     $document_root = '/var/www/html/current';
-    if (file_exists($document_root . "site-configuration.json")) {
+    if (file_exists($document_root . "/site-configuration.json")) {
       $configuration_string = file_get_contents($document_root . "/site-configuration.json");
     } else { // Use non-production
       $configuration_string = file_get_contents($document_root . "/site-configuration.json.example");
